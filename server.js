08d48bb8e5b6812b173a9fb1c487addeee94db59
Jady Hom <hyjady@gmail.com>
2020-11-07 18:31:32 -0500

    added middleware

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const connection = require('./db/index')
 
 const PORT = process.env.PORT || 3001
+const app = express()
 
 // MIDDLEWARE
-
+app.use(logger('dev'))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 // MIDDLEWARE
 
 app.use('/', (request, response) => {

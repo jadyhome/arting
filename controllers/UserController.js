b3c7de28a6ba4f9c4f048ff69879acaba22df6ad
Jady Hom <hyjady@gmail.com>
2020-11-07 18:27:12 -0500

    setup UserController

---
+++ controllers/UserController.js
@@ -0,0 +1,51 @@
+const User = require('../models/User')
+const Artboard = require('../models/ArtBoard')
+
+const UserSignUp = async (request, response) => {
+  try {
+    const body = request.body
+    const user = new User({
+        name: body.name,
+        user_name: body.user_name,
+        email: body.email,
+        password_digest: body.password
+    })
+    user.save()
+    response.send(user)
+  } catch (error) {
+    throw error
+  }
+}
+
+const UserSignIn = async (request, response) => {
+  try {
+    const user = await User.findOne({ user_name: request.body.user_name })
+    if (user && user.password_digest === request.body.password_digest) {
+      const payload = {
+        name: user.name,
+        user_name: user.user_name
+      }
+      response.send(payload)
+    }
+    response.status(401).send({ Error: "Unauthorized" })
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetArtBoard = async (request, response) => {
+  try {
+    const user = await User.findOne({ user_name: request.params.user_name })
+    const board = await Artboard.find({ user_name: request.params.user_name })
+    response.send({ user, board })
+  } catch (error) {
+    throw error
+  }
+}
+
+
+module.exports = {
+    UserSignUp,
+    UserSignIn,
+    GetArtBoard
+}

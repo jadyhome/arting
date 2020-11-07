96753e7bdc16ed938bd02fb2432ff582280a63e0
Jady Hom <hyjady@gmail.com>
2020-11-07 15:59:39 -0500

    setup artboard schema

---
+++ models/artboard.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose')
+
+const ArtBoard = new Schema(
+  {
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'users'
+    },
+    board_name: {
+      type: String,
+      required: true
+    },
+    image_url: {
+        type: String,
+        required: true
+    },
+    likes: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    views: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    comments: [{
+      type: Schema.Types.ObjectId,
+      ref: 'comments'
+    }]
+  },
+  { timestamps: true }
+)
+
+module.exports = model('artboards', ArtBoard)

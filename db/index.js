047ea6a34ed54757b8cb07f921f5807d082623bd
Jady Hom <hyjady@gmail.com>
2020-11-08 18:55:38 -0500

    fixed connection database

--- db/index.js
+++ db/index.js
@@ -1,18 +1,12 @@
 const mongoose = require('mongoose')
 
-mongoose
-  .connect('mongodb://127.0.0.1:27017/artBoardDatabase', { 
-    useUnifiedTopology: true, 
-    useNewUrlParser: true 
-  })
-  .then(() => {
-    console.log('Successfully connected to MongoDB.');
-  })
-  .catch(error => {
-    console.error('Connection error', error.message)
-  })
+const connection = mongoose.connect('mongodb://localhost:27017/artBoardDatabase', {
+  useNewUrlParser: true,
+  useFindAndModify: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
 
 mongoose.set('debug', true)
-const db = mongoose.connection
 
-module.exports = db
+module.exports = connection

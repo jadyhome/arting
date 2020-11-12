cc2c2098b4c084668377e2ec7586c9124bc632d0
Jady Hom <hyjady@gmail.com>
2020-11-12 12:25:45 -0500

    setup basic NavLink

---
+++ client/src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default () => {
+  return (
+    <nav>
+      <NavLink activeClassName="nav-active" to="/portfolios">
+        portfolios
+      </NavLink>
+      <NavLink activeClassName="nav-active" to="/signup">
+        sign up
+      </NavLink>
+      <NavLink activeClassName="nav-active" to="/signin">
+        sign in
+      </NavLink>
+    </nav>
+  )   
+}

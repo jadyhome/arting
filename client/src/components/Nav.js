e9bf84a81498f08c6412f56a1c03af0a94712fde
Jady Hom <hyjady@gmail.com>
2020-11-13 12:07:18 -0500

    added another classname for arting route

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 export default () => {
   return (
     <nav>
-      <NavLink activeClassName="nav-active" to="/">
+      <NavLink activeClassName="title nav-active" to="/">
         arting
       </NavLink>
       <NavLink activeClassName="nav-active" to="/portfolios">

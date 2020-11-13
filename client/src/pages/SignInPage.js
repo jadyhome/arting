3851d412f8a1839ee3452790473cc70862f05818
Jady Hom <hyjady@gmail.com>
2020-11-12 19:41:46 -0500

    imported css file and signin and signup component

--- client/src/pages/SignInPage.js
+++ client/src/pages/SignInPage.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import SignIn from '../components/SignIn'
+import '../styles/SignInUp.css'
 
 const SignInPage = () => {
   return (
     <div className="signin-page">
       <section className="navbar">
-          <Nav />
+        <Nav />
       </section>
 
-      <div className="signin-message">
-        <h1>Welcome Back!</h1>
+      <div className="signin-left">
+        <h2 className="welcomeback">welcome back!</h2>
+      </div>
+
+      <div className="signin-form">
+        <SignIn />
       </div>
     </div>
   )
--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import SignUp from '../components/SignUp'
+import '../styles/SignInUp.css'
 
 const SignUpPage = () => {
   return (
     <div className="signup-page">
       <section className="navbar">
-          <Nav />
+        <Nav />
       </section>
       
-      <div className="signup-message">
+      <div className="signup-left">
+        <div className="artingmessage">
         <h1>arting</h1>
         <h2>[ ar-ting ]</h2>
-        <h2>verb</h2>
+        <h3><em>verb</em></h3>
         <h3>1. the action of doing art</h3>
         <h3><em>i am arting right now.</em></h3>
+        </div>
+      </div>
+
+      <div className="signup-form">
+        <SignUp />
       </div>
     </div>
   )

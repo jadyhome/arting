08c3e55ffc3c270af8326b3e54fd70cefc715b8b
Jady Hom <hyjady@gmail.com>
2020-11-14 14:15:33 -0500

    added middleware to UserRouter

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,8 +1,10 @@
 const Router = require('express').Router()
 const UserController = require('../controllers/UserController')
+const { getToken, createToken, verifyToken } = require('../middleware/JwtHandler')
 
 Router.post('/signup', UserController.UserSignUp)
-Router.post('/signin', UserController.UserSignIn)
+Router.post('/signin', UserController.UserSignIn, createToken)
 Router.get('/:user_id', UserController.GetProfile)
+Router.get('/refresh/session', getToken, verifyToken, UserController.RefreshSession)
 
 module.exports = Router

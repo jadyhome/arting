9365fdff27839231f61f7ff0ff8f99f10b0cac14
Jady Hom <hyjady@gmail.com>
2020-11-14 18:51:06 -0500

    added and styled mainpage

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -10,6 +10,16 @@ const MainPage = ({ children }) => {
           <Nav />
           {children}
       </section>
+
+      <div className="main-content">
+        <div className="artingmessage">
+        <h1>arting</h1>
+        <h2>[ ar-ting ]</h2>
+        <h3><em>verb</em></h3>
+        <h3>1. the action of doing art</h3>
+        <h3><em>i am arting right now.</em></h3>
+        </div>
+      </div>
     </div>
   )
 }

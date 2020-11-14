5d45afbd1233e5fb087552a35192b34f26556da3
Jady Hom <hyjady@gmail.com>
2020-11-14 14:01:32 -0500

    setup basic profile component

---
+++ client/src/components/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Profile = () => {
+    return (
+        <div className="profile">
+          <div className="profile-icon">
+            a box here
+          </div>
+          <h3>username</h3>
+        </div>
+    )
+  }
+
+export default Profile

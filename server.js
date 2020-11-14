959becedaadd9de101c61716fbb5562c5bde1854
Jady Hom <hyjady@gmail.com>
2020-11-14 14:05:02 -0500

    adding helmet to server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const helmet = require('helmet')
 const connection = require('./db/connection')
 
 const PORT = process.env.PORT || 3001
@@ -10,11 +11,13 @@ const app = express()
 
 // MIDDLEWARE
 app.use(logger('dev'))
+app.use(helmet())
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 // MIDDLEWARE
 
+app.disable('X-Powered-By')
 app.get('/', (request, response) => {
   response.send({ Message: 'This is Home Route.' })
 })

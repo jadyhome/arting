86d6a91f1e9dcce8bdffd991d90406053e5d999b
Jady Hom <hyjady@gmail.com>
2020-11-14 14:32:14 -0500

    integrated auth to apiclient

--- client/src/services/ApiClient.js
+++ client/src/services/ApiClient.js
@@ -2,5 +2,15 @@ import axios from 'axios'
 
 const ApiClient = axios.create({ baseURL: 'http://localhost:3001/api' })
 
+ApiClient.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
 
 export default ApiClient

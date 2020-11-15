f3a9dcd9dd451abf1a129df36b22f91423150e9f
Jady Hom <hyjady@gmail.com>
2020-11-15 15:23:02 -0500

    edited signedinlayout

--- client/src/components/SignedInLayout.js
+++ client/src/components/SignedInLayout.js
@@ -1,30 +1,16 @@
 import React from 'react'
 import Nav from './Nav'
-
-import Board from './Board'
 import '../styles/Board.css'
 import '../styles/ProfilePage.css'
 
 const SignedInLayout = ({ children, authenticated, currentUser }) => (
   <div>
-      <div className="profilepage">
-      <section className="navbar">
-      <Nav 
-      authenticated={authenticated}
-      currentUser={currentUser}
-      />
-         
-      </section>
-
-
-      <div className="artboards-container">
-      <Board />
-      <Board />
-      <Board />
-
-      </div>
-    </div>
-    
+    <section className="navbar">
+    <Nav 
+    authenticated={authenticated}
+    currentUser={currentUser}
+    />
+    </section>
     {children}
   </div>
 )

0d858b1ab0c8baaa9ade3dd68bc16b2149925bd5
Jady Hom <hyjady@gmail.com>
2020-11-15 15:23:18 -0500

    fixed errors in profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import Nav from '../components/Nav'
+import { Link } from 'react-router-dom'
 import Board from '../components/Board'
-import Create from '../components/Create'
 import { __GetProfile } from '../services/UserService'
+import { __DeleteBoard } from '../services/BoardService'
 import '../styles/Board.css'
 import '../styles/ProfilePage.css'
 
@@ -16,41 +16,93 @@ class Profile extends Component {
   }
 
   componentDidMount() {
-    this.getBoards()
+    this.getProfile()
   }
 
-  getBoards = async () => {
+  getProfile = async () => {
     try {
+      console.log(this.props)
       const profile = await __GetProfile(this.props.currentUser._id)
-      this.setState({ boards: profile.boards })
+      console.log('profile', profile)
+      this.setState({ boards: profile.board })
     } catch (error) {
       this.setState({ boardFetchError: true })
     }
   }
 
+  deleteBoard = async (id) => {
+    try {
+      const boardsToKeep = this.state.boards.filter((board) => board._id !== id)
+      this.setState({ boards: boardsToKeep })
+      await __DeleteBoard(id)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   render() {
+    // const { name } = this.state
     return (
       <div className="profilepage">
+        
         <div className="profile">
           <div className="profile-icon">
             a box here
           </div>
-          <h3>username</h3>
+          <h3>
+            name
+            </h3>
+          <div className="create">
+            <Link to="/create">create</Link>
+          </div>
         </div>
-        <section className="navbar">
-          <Nav />
+ 
+        <div className="artboards">
+        
+        <div className="boardcontainer">
+          
+          {this.state.boards.length ? (
+            <div className="board-content wrapper flex-row">
+              {this.state.boards.map((board) => (
+                <div key={board._id}>
+                  <Board
+                    onClick={() =>
+                      this.props.history.push(`/artboards/${board._id}`)
+                    }
+                  >
+                      <div className="board-content">
+                        <h3>{board.title}</h3>
+                        
+                      </div>
+                    
+                    <img src={board.image_url} alt="artboards" />
+                  </Board>
 
-        </section>
-
-
-        <div className="artboards-container">
-          <Board />
-          <Board />
-          <Board />
-
+                  {/* <div className="flex-row button-wrapper">
+                    <button
+                      onClick={() =>
+                        this.props.history.push(`/edit/${board._id}`)
+                      }
+                    >
+                      Edit
+                    </button>
+                    <button onClick={() => this.deleteBoard(board._id)}>
+                      Delete
+                    </button>
+                  </div> */}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="span message">No ArtBoards</div>
+          )}
         </div>
+
+        
+      </div>
       </div>
     )
   }
 }
+
 export default Profile

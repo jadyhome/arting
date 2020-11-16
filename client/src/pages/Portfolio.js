59b334fb80134c2205489fdf6d578e339ba45da0
Jady Hom <hyjady@gmail.com>
2020-11-15 19:27:51 -0500

    deleted old portfolio and setup new js file

---
+++ client/src/pages/Portfolio.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import Board from '../components/Board'
+import Nav from '../components/Nav'
+import { __GetArtBoards } from '../services/BoardService'
+import '../styles/Navbar.css'
+import '../styles/PortfolioPage.css'
+
+class Portfolio extends Component {
+  constructor() {
+    super()
+    this.state = {
+      boards: []
+    }
+  }
+
+  componentDidMount() {
+    this.getBoards()
+  }
+
+  getBoards = async () => {
+    try {
+      const boards = await __GetArtBoards()
+      this.setState({ boards: [...this.state.boards, ...boards] })
+    } catch (error) {
+      throw error
+    }
+  }
+
+  render() {
+    const { boards } = this.state
+    return (
+      <div className="portfoliopage">
+        <section className="navbar">
+          <Nav />
+        </section>
+        
+        <div className="portfolios">
+          <h3>works</h3>
+          <section className="all-content">
+            {boards.length ? (
+              boards.map((board) => (
+                <Board onClick={() =>
+                  this.props.history.push(`/artboards/${board._id}`)
+                }>
+                  <img src={board.image_url} alt="artboards" />
+                  <div className="board-title">
+                    <h4>{board.title}</h4>    
+                  </div>
+                  <p>♥ {board.likes}</p>
+                  <p>👀 {board.views}</p>
+                  <p>comments {board.comments}</p>
+                </Board>
+              ))
+            ) : (
+              <h3>No ArtBoards</h3>
+            )}
+          </section>
+        </div>
+      </div>
+    )
+
+  }
+}
+
+export default Portfolio
